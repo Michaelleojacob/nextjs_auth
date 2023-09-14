c30a0bd83dddbd964beb938a380d52609f4e7b1d
mig <michaelleojacob@gmail.com>
2023-09-14 15:53:19 -0600

    deleted database and setup routes and http tests to delete

--- pages/api/deleteAllData.ts
+++ pages/api/deleteAllData.ts
@@ -1,6 +1,11 @@
 import prisma from "../../prisma/prisma";
+import { NextApiRequest, NextApiResponse } from "next";
 
-export default async function deleteUsers() {
+export default async function deleteUsers(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   await prisma.user.deleteMany({});
   await prisma.post.deleteMany({});
+  return res.status(200).send("all data deleted.");
 }
