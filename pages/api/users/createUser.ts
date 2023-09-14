2dca78a4164664787a0e83058ed926d2bb1d30d9
mig <michaelleojacob@gmail.com>
2023-09-14 16:13:37 -0600

    added hash to create user

--- pages/api/users/createUser.ts
+++ pages/api/users/createUser.ts
@@ -1,21 +1,25 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import prisma from "../../../prisma/prisma";
+import bcrypt from "../../bycrypt";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  if (req.method === "POST") {
-    try {
-      const user = await prisma.user.create({
-        data: {
-          name: req.body.name,
-          password: req.body.password,
-        },
-      });
-      return res.status(200).send(user);
-    } catch (e) {
-      return res.status(400).send("no user created");
-    }
+  try {
+    const noHash = req.body.password;
+    const saltRounds = 10;
+    const salt = bcrypt.genSaltSync(saltRounds);
+    const hash = bcrypt.hashSync(noHash, salt);
+
+    const user = await prisma.user.create({
+      data: {
+        name: req.body.name,
+        password: hash,
+      },
+    });
+    return res.status(200).send(user);
+  } catch (e) {
+    return res.status(400).send("no user created");
   }
 }
