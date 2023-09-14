9ce2d7bbdb27db350ade3d155476281dd701c19f
mig <michaelleojacob@gmail.com>
2023-09-14 16:36:57 -0600

    added sanitization to createUser via escape and trim

--- pages/api/users/createUser.ts
+++ pages/api/users/createUser.ts
@@ -1,13 +1,18 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import prisma from "../../../prisma/prisma";
 import bcrypt from "../../bycrypt";
+import validator from "validator";
+
+// validate on front end for length
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   try {
-    const noHash = req.body.password;
+    const name = validator.trim(validator.escape(req.body.name));
+    const password = validator.trim(validator.escape(req.body.password));
+    const noHash = password;
     const saltRounds = 10;
     const salt = bcrypt.genSaltSync(saltRounds);
     const hash = bcrypt.hashSync(noHash, salt);
