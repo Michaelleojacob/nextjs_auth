2608a2de57227472972e20ddb32cc2d150c00447
mig <michaelleojacob@gmail.com>
2023-09-14 15:45:19 -0600

    added try catch to superUser/create.ts

--- pages/api/users/superUser/create.ts
+++ pages/api/users/superUser/create.ts
@@ -7,17 +7,21 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  if (!process.env.MIGPASS) return;
-  const noHash = process.env.MIGPASS;
-  const saltRounds = 10;
-  const salt = bcrypt.genSaltSync(saltRounds);
-  const hash = bcrypt.hashSync(noHash, salt);
+  if (!process.env.MIGPASS) return res.status(400).send("error creating mig");
+  try {
+    const noHash = process.env.MIGPASS;
+    const saltRounds = 10;
+    const salt = bcrypt.genSaltSync(saltRounds);
+    const hash = bcrypt.hashSync(noHash, salt);
 
-  const superUser = await prisma.user.create({
-    data: {
-      name: "Migs",
-      password: hash,
-    },
-  });
-  return res.send(superUser);
+    const superUser = await prisma.user.create({
+      data: {
+        name: "Migs",
+        password: hash,
+      },
+    });
+    return res.send(superUser);
+  } catch (e) {
+    return res.status(400).send(e);
+  }
 }
